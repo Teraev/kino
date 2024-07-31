3c593d9508a5c9f69bca1cb06315184c911fcf12
teraevradmir100@gmail.com <teraevradmir100@gmail.com>
2024-07-31 15:05:49 +0500

    commit

--- src/components/all_trelers.ts
+++ src/components/all_trelers.ts
@@ -1,7 +1,7 @@
 import { getData } from "../lib/http.request";
 import { video } from "../main";
 
-export function createTrelers(item:any) {
+export function createTrelers(item: any) {
   const movieCard = document.createElement('div');
   movieCard.classList.add('trelers');
 
@@ -12,22 +12,22 @@ export function createTrelers(item:any) {
   const title = document.createElement('h3');
   title.classList.add('movie-title');
   title.textContent = item.title;
-  
+
   movieCard.append(img, title);
 
   movieCard.onclick = () => {
     getData(`/movie/${item.id}/videos`)
-    .then(res => {
-      if (res.status === 200) {
-        const Trailer = res.data.results.find(video => video.type === 'Trailer')
-        video.src = `https://www.youtube.com/embed/${Trailer.key}`;
-        
-      }
-    })
+      .then(res => {
+        if (res.status === 200) {
+          const Trailer = res.data.results.find(video => video.type === 'Trailer')
+          video.src = `https://www.youtube.com/embed/${Trailer.key}`;
+
+        }
+      })
     tralerName.innerText = item.title;
   }
-  
-  
+
+
   return movieCard
 }
 
